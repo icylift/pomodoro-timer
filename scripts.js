5b465bce9ef722ba0502a3684d9de0943ae21133
Aaron Williams <polarizedcuriosity@gmail.com>
2023-10-22 10:48:51 -0700

    working on the javascript functionality for the timer

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@
 let workTitle = document.getElementById("work");
 let breakTitle = document.getElementById("break");
 
-let workTime = 25;
+let workTime = 1;
 let breakTime = 5;
 
 let seconds = "00";
@@ -28,8 +28,35 @@ function start() {
 
   // countdown
 
-  let timerFunction = () => {};
+  let timerFunction = () => {
+    // change the display
+    document.getElementById("minutes").innerHTML = workMinutes;
+    document.getElementById("seconds").innerHTML = seconds;
 
-  // start countdown
-  setInterval(timerFunction, 1000); // 1000 = 1s
+    // start
+    seconds = seconds - 1;
+  };
+
+  if (seconds === 0) {
+    workMinutes = workMinutes - 1;
+    if (workMinutes === -1) {
+      if (breakCount % 2 === 0) {
+        // start break
+        workMinutes = breakMinutes;
+        breakCount++;
+
+        // change the painel
+        workTitle.classList.remove("active");
+        breakTitle.classList.remove("active");
+      } else {
+        // continue work
+        workMinutes = workTime;
+        breakCount++;
+      }
+    }
+    seconds = 59;
+  }
 }
+
+// start countdown
+setInterval(timerFunction, 1000); // 1000 = 1s
